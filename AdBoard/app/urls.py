3ce5e7991fa2d3263a7bddef824e0d96527a223d
Soho36 <vladimir.sehirev@gmail.com>
2024-08-20 01:02:38 +0300

    categories linking added to navabar

--- AdBoard/app/urls.py
+++ AdBoard/app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PostsList, PostDetail, PostByCategory
 
 urlpatterns = [
-    path('', PostsList.as_view(), name='post_list'),
+    path('', PostsList.as_view(), name='post_list'),    # post_list in this case is the name of the url pattern
     path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
     path('<str:category_name>/', PostByCategory.as_view(), name='posts_by_category'),
 ]
